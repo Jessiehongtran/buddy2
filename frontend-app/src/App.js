e6358cd13a666c86e7a01e7e1f7cd47ca3a343d9
Your Name <you@example.com>
2020-05-07 11:53:13 -0700

    added a button to find partner and body background

---
+++ frontend-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Find a partner</button>
+    </div>
+  );
+}
+
+export default App;
