aafbff051a1d2f40e7bd33a1b672c3a98a9076c4
Your Name <you@example.com>
2020-07-11 21:57:38 -0700

    linked time routes

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const dayRoutes = require('./routes/days.route');
 const requestRoutes = require('./routes/requests.route');
 const topicRoutes = require('./routes/topics.route');
 const userRoutes = require('./routes/users.route');
+const timeRoutes = require('./routes/times.route');
 
 
 //middlewares
@@ -18,6 +19,7 @@ app.use('/api/days', dayRoutes)
 app.use('/api/requests', requestRoutes)
 app.use('/api/topics', topicRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/times', timeRoutes)
 
 
 
