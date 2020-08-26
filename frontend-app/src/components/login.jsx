3cf669ae940d7a830ef32388064d46589e583f53
Your Name <you@example.com>
2020-08-26 12:27:21 -0700

    styled sign in for web

--- frontend-app/src/components/login.jsx
+++ frontend-app/src/components/login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
+import '../styles/login.scss'
 import { API_URL } from '../config';
 
 export default class Login extends React.Component {
@@ -41,21 +42,31 @@ export default class Login extends React.Component {
 
     render(){
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    placeholder="Email"
-                    type="email" 
-                    name="email"
-                    onChange={this.handleChange}
-                />
-                <input
-                    placeholder="Password"
-                    type="password" 
-                    name="password"
-                    onChange={this.handleChange}
-                />
-                <button>Log In</button>
-            </form>
+            <div className="login">
+                
+                <form onSubmit={this.handleSubmit}>
+                    <p>Sign In</p>
+                    <div className="input-container">
+                        <i class="fa fa-envelope icon"></i>
+                        <input
+                            placeholder="Email"
+                            type="email" 
+                            name="email"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="input-container">
+                        <i class="fa fa-key icon"></i>
+                        <input
+                            placeholder="Password"
+                            type="password" 
+                            name="password"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button>Log In</button>
+                </form>
+            </div>
         )
     }
 }
