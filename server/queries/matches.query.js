f6a1de552efa4ce7356b65cd8c068640a1c610c7
Hong Tran <jessietran@fb.com>
2021-02-18 06:20:45 -0800

    remigrated

--- server/queries/matches.query.js
+++ server/queries/matches.query.js
@@ -2,6 +2,7 @@ const db = require('../database/dbConfig');
 
 const getMatchByRequestID = (requestID) => {
     return db('matches')
+            // .where('request1_id', requestID)
             .where(function(){
                 this.where('request1_id', requestID).orWhere('request2_id', requestID)
             })
